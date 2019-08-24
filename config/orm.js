46bb8c51b74946498b79cf444d5dc33cb10aca39
Casey Carroll <caseydean8@gmail.com>
2019-08-24 11:57:27 -0400

    server returns first orm function

--- config/orm.js
+++ config/orm.js
@@ -2,23 +2,25 @@
             // can this be ('./connection')?
 const connection = require('./connection.js')
 
+// orm is a constructor?
 const orm = {
-    selectAll: function(table) {
+    select: function(table) {
         const queryString = "SELECT * FROM ??";
         connection.query(queryString, [table], function(err, result) {
             if (err) throw err;
             console.log(result);
         });
     },
-    insert1: function(table, column) {
+    insert1: function(table, column1, column2) {
         const queryString = "INSERT INTO ?? VALUES ?";
-        connection.query(queryString, [table, column], function(err, result) {
+        connection.query(queryString, [table, column1, column2], function(err, result) {
             if (err) throw err;
             console.log(result);
         });
     },
     update1: function(table, column, column2, id) {
         //UPDATE table1 SET col_a='k1', col_b='foo' WHERE key_col='1';
+// will likely need to build a function with a for loop to handle the comma between ?? below
         const queryString = "UPDATE ?? SET ??, ??,  WHERE ??";
         connection.query(queryString, [table, column, column2, id], function(err, result) {
             if (err) throw err;
--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 const express = require('express')
+
+const orm = require('./config/orm.js')
+
+orm.select('burgers')
+
+// orm.insert('burgers', '',)
+
+// orm.update1('', '', '', '')
